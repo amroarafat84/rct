71de76089c013f8fe51e3cdbb23bfc8568fd7a47
SimoneMineo <simone.mineo424@gmail.com>
2019-04-10 12:16:08 -0400

    tempcommit

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,10 +1,33 @@
 package com.revature.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.models.User;
+import com.revature.service.UserService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/user")
 public class UserController {
-
+	
+	@Autowired
+	UserService uService; //SHOULD BE SERVICE LAYER to follow best practices
+	
+	//ADD USER
+	@RequestMapping(name="/register",
+			method=RequestMethod.POST, 
+			consumes=MediaType.APPLICATION_JSON_VALUE,
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> add(@RequestBody User user){
+		user = uService.addUser(user);
+		return new ResponseEntity<User>(user, HttpStatus.CREATED);
+	}
 }
--- src/main/java/com/revature/service/UserService.java
+++ src/main/java/com/revature/service/UserService.java
@@ -1,5 +1,33 @@
 package com.revature.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.models.User;
+import com.revature.repositories.UserRepository;
+
+@Service
+@Transactional
 public class UserService {
 
+	@Autowired
+	private UserRepository userRepo;
+	
+	
+	public User addUser(User u) {
+		if(u.getUsername() == null || u.getPassword() == null) {
+			return null;
+		}
+		else if(userRepo.findByUsernameIgnoreCase(u.getUsername())!= null) {
+			//username already exists
+			return null;
+		}
+		else {
+			return userRepo.save(u);
+		}
+		
+	}
+
+	//pretend this has functionality ....
 }
