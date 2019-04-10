8b1e5a705056d68fe837af3704aa3059ea31ea41
Amro Arafat <amroarafat84@gmail.com>
2019-04-10 10:07:29 -0400

    fix conflicts

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,5 +1,10 @@
 package com.revature.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
 }
