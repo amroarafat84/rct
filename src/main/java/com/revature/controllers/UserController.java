c694279607ac59c2ce388e677173754a442d9859
Amro Arafat <amroarafat84@gmail.com>
2019-04-11 12:53:59 -0400

    Adding line in user controller

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,12 +1,9 @@
 package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-=======
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
->>>>>>> 5e0bb82291f404d3038a70a126962ebc90149fe0
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,27 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.models.User;
-<<<<<<< HEAD
-import com.revature.repositories.UserRepository;
-
-@RestController
-@RequestMapping("/users")
-@CrossOrigin
-public class UserController {
-	
-	@Autowired
-	UserRepository userRepo;
-	
-	@RequestMapping(value="/register", 
-			method = RequestMethod.POST, 
-			headers = {"content-type=application/json"})
-	public User register(@RequestBody User user) {
-		System.out.println(user);
-		return userRepo.save(user); 
-	}
-
-=======
 import com.revature.service.UserService;
+
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -59,8 +37,7 @@ public class UserController {
 	public ResponseEntity<User> log(@RequestBody User user){
 		user=uService.login(user);
 		if(user!=null) {
-		return new ResponseEntity<User>(user, HttpStatus.I_AM_A_TEAPOT);}
+		return new ResponseEntity<User>(user, HttpStatus.OK);}
 		else {return new ResponseEntity<User>(HttpStatus.NO_CONTENT);}
 	}
->>>>>>> 5e0bb82291f404d3038a70a126962ebc90149fe0
 }
